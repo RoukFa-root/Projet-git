2049333858b33b6cf328f9924cae19b58edaa1a1
Ril <rimtourabi@gmail.com>
2022-09-29 16:45:26 +0100

    Change: Dynamic Repository Git

--- script2.sh
+++ script2.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-repository="git@github.com:RoukFa-root/TP-versionning-git.git"
+#repository="git@github.com:RoukFa-root/TP-versionning-git.git"
 
 #localFolder="/Documents/ESGI/Versionning GIT/projet/clone"
 
-git clone "$repository" "$1"
+git clone "$1" "$2"
 
